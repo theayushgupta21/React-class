6fdb1e63f17bba395f2d5e8caa9c1fb175f7ff3e
Ayush Gupta <ayushgupta9510@gmail.com>
2026-03-20 00:07:24 +0530

    Merge unrelated histories

---
+++ new class/script.js
@@ -0,0 +1,14 @@
+// NPM Understanding
+
+// text to speech
+// installing and uninstalling anything basic& advanced
+// installing particular versions 
+//npm install (i) packagename
+//understanding node_modules
+
+
+//devdependencies - jo packages development me kaam ayenge or jab develop hojayega tab or upload hojayega tab wo kaam nhi ayega
+
+//scripts- understanding default scripts Path and coustom sripts
+
+console.log("hello"); 
